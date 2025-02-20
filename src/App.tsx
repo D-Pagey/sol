dbd4e64a912663678fe821ecf6712d8858baadbb
Dan Page <26827986+D-Pagey@users.noreply.github.com>
2025-02-20 21:56:00 +0000

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import sol from "/sol.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <img src={sol} className="logo" alt="Sol" />
+      <h1>Sol</h1>
+      <p>The component library for those that can't finish their drinks</p>
+    </div>
+  );
+}
+
+export default App;
